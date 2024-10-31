106cd3f38943288bcc97559e9e43a5516c8a6df3
hari balaji <145361290+haribalji@users.noreply.github.com>
2024-10-31 11:39:37 +0530

    Updates

---
+++ precache-manifest.0403bba0f3da6fdbf67fe5573f276ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1239f2b2576574143360c52fa50125b5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "af564be723fac4be3539",
+    "url": "/my-app/static/css/main.c2d977e3.chunk.css"
+  },
+  {
+    "revision": "8ea57846da87c05e2447",
+    "url": "/my-app/static/js/2.f8a3d5ae.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/my-app/static/js/2.f8a3d5ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82594277ffdbbbbe6c96",
+    "url": "/my-app/static/js/3.f00e9902.chunk.js"
+  },
+  {
+    "revision": "af564be723fac4be3539",
+    "url": "/my-app/static/js/main.3317c0be.chunk.js"
+  },
+  {
+    "revision": "5549b0f24f1d0a4d74f6",
+    "url": "/my-app/static/js/runtime-main.48e1deef.js"
+  }
+]);
